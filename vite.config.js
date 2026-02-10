36947e85a086f7adeb59e5c4f0169d4361d36444
NandaDattaBuri <nanda.b@vcloudmaster.com>
2026-02-10 11:44:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',  // Must be './' for Amplify
+  base: './',
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        format: 'iife',  // Disable module scripts
-        inlineDynamicImports: true
-      }
-    }
-  },
-  // For local development only
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://blogbackend-yq0v.onrender.com',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    outDir: 'dist'
   }
 })
