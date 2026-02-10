67c74ee7bafdda1d5d18d1e0bf44c51715cbdac6
NandaDattaBuri <nanda.b@vcloudmaster.com>
2026-02-10 13:39:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
+  base: '/',  // ← Change './' to '/'
   build: {
     outDir: 'dist'
   }
 })
-
-
